57b7722b345503f0267ba3d1f411a1f6808c56a0
Endless <0x3f@0x3f3f3f3f.com>
2026-02-18 20:47:32 +0800

    cf2040D

---
+++ main/2000-2099/2040D.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf2040D(in io.Reader, _w io.Writer) {
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+	const mx int = 2e5 + 1
+	primes := []int{}
+	np := [mx]bool{1: true}
+	for i := 2; i < mx; i++ {
+		if !np[i] {
+			primes = append(primes, i)
+			for j := i * i; j < mx; j += i {
+				np[j] = true
+			}
+		}
+	}
+
+	var T, n int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n)
+		g := make([][]int, n)
+		for range n - 1 {
+			var v, w int
+			Fscan(in, &v, &w)
+			v--
+			w--
+			g[v] = append(g[v], w)
+			g[w] = append(g[w], v)
+		}
+
+		ans := make([]int, n)
+		ans[0] = 1
+		cur := 2
+		var dfs func(int, int)
+		dfs = func(v, fa int) {
+			for _, w := range g[v] {
+				if w == fa {
+					continue
+				}
+				for !np[cur-ans[v]] {
+					cur++
+				}
+				ans[w] = cur
+				cur++
+				dfs(w, v)
+			}
+		}
+		dfs(0, -1)
+		for _, v := range ans {
+			Fprint(out, v, " ")
+		}
+		Fprintln(out)
+	}
+}
+
+//func main() { cf2040D(bufio.NewReader(os.Stdin), os.Stdout) }
