9ac716fde2de9791a5169767151e13cb5af13ae3
Endless <0x3f@0x3f3f3f3f.com>
2026-02-25 08:17:13 +0800

    cf1835C

--- main/1800-1899/1835C.go
+++ main/1800-1899/1835C.go
@@ -14,33 +14,35 @@ func cf1835C(in io.Reader, _w io.Writer) {
 	var T, k int
 	for Fscan(in, &T); T > 0; T-- {
 		Fscan(in, &k)
-		half := 1 << k // 值域的一半
-		s := make([]int, half*2+1)
-		pos := make([]int, half)
-		for i := range pos {
-			pos[i] = -1
-		}
-		pos[0] = 0
-		pos2 := make([][2]int, half)
-		for i := 1; ; i++ {
+		n := 1 << (k + 1)
+		s := make([]int, n+1)
+		for i := 1; i <= n; i++ {
 			Fscan(in, &s[i])
 			s[i] ^= s[i-1]
-			si := s[i]
-			j := pos[si&(half-1)]
+		}
+
+		half := n / 2
+		last := make([]int, half)
+		for i := range last {
+			last[i] = -1
+		}
+		last[0] = 0
+		pos := make([][2]int, half)
+
+		for i := 1; ; i++ {
+			low := s[i] & (half - 1)
+			j := last[low]
 			if j >= 0 {
-				s2 := (si ^ s[j]) >> k
-				if pos2[s2][1] > 0 {
-					a := append(pos2[s2][:], j)
-					slices.Sort(a)
-					Fprintln(out, a[0]+1, a[1], a[2]+1, i)
-					for range half*2 - i {
-						Fscan(in, &k)
-					}
+				high := (s[i] ^ s[j]) >> k
+				if pos[high][1] > 0 {
+					p := append(pos[high][:], j)
+					slices.Sort(p)
+					Fprintln(out, p[0]+1, p[1], p[2]+1, i)
 					break
 				}
-				pos2[s2] = [2]int{j, i}
+				pos[high] = [2]int{j, i}
 			}
-			pos[si&(half-1)] = i
+			last[low] = i
 		}
 	}
 }
