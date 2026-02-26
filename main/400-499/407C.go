634196023a2df5931db696c9a33585c6f1fec858
Endless <0x3f@0x3f3f3f3f.com>
2026-02-26 09:58:12 +0800

    cf407C

--- main/400-499/407C.go
+++ main/400-499/407C.go
@@ -18,15 +18,16 @@ func cf407C(in io.Reader, _w io.Writer) {
 		Fscan(in, &a[i])
 	}
 
-	c := make([][101]int, n+101)
+	const mx = 100
+	c := make([][mx + 1]int, n+mx+1)
 	for i := range c {
 		c[i][0] = 1
-		for j := 1; j <= min(i, 100); j++ {
+		for j := 1; j <= min(i, mx); j++ {
 			c[i][j] = (c[i-1][j] + c[i-1][j-1]) % mod
 		}
 	}
 
-	f := [102][]int{}
+	f := [mx + 2][]int{}
 	for i := range f {
 		f[i] = make([]int, n+2)
 	}
@@ -37,7 +38,7 @@ func cf407C(in io.Reader, _w io.Writer) {
 			f[j][r+1] = (f[j][r+1] - c[r-l+k-j][k-j]) % mod
 		}
 	}
-	for i := 100; i >= 0; i-- {
+	for i := mx; i >= 0; i-- {
 		for j := 1; j <= n; j++ {
 			f[i][j] = (f[i][j] + f[i][j-1] + f[i+1][j]) % mod
 		}
