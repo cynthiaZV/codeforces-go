bff1e3919fccbac80ca7cc4004004e5cc46b6c03
Endless <0x3f@0x3f3f3f3f.com>
2026-02-28 09:18:00 +0800

    lc1680

--- leetcode/weekly/218/c/c.go
+++ leetcode/weekly/218/c/c.go
@@ -2,12 +2,40 @@ package main
 
 import "math/bits"
 
-// todo O(log n) ?
-
 // github.com/EndlessCheng/codeforces-go
-func concatenatedBinary(n int) (ans int) {
+func concatenatedBinary1(n int) (ans int) {
+	const mod = 1_000_000_007
 	for i := 1; i <= n; i++ {
-		ans = (ans<<bits.Len(uint(i)) | i) % (1e9 + 7)
+		w := bits.Len(uint(i))
+		ans = (ans<<w | i) % mod
 	}
 	return
+}
+
+// github.com/EndlessCheng/codeforces-go
+const mod = 1_000_000_007
+
+func pow(x, n int) int {
+	res := 1
+	for ; n > 0; n /= 2 {
+		if n%2 > 0 {
+			res = res * x % mod
+		}
+		x = x * x % mod
+	}
+	return res
+}
+
+func concatenatedBinary(n int) (ans int) {
+	for w := 1; 1<<(w-1) <= n; w++ {
+		l := 1 << (w - 1)
+		r := min(1<<w-1, n)
+		m := r - l + 1
+		q := 1 << w
+		powQ := pow(q, m)
+		invQ1 := pow(q-1, mod-2)
+		s := r*(powQ-1)%mod*invQ1 - (q-m*powQ+(m-1)*powQ%mod*q)%mod*invQ1%mod*invQ1
+		ans = (ans*powQ + s) % mod
+	}
+	return (ans + mod) % mod // 保证结果非负
 }
